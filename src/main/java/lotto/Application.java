a393ca2a5aabf136af88c022bc0c3bc8c82a1bc4
unknown <kim0615157@gmail.com>
2025-10-30 09:06:59 +0900

    feat:로또 구입금액 입력
    구입금액이 숫자인지확인
    
    구입 금액이 1,000원 단위로 나누어 떨어지는지 확인

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("구입금액을 입력해 주세요.");
+        String inputLottoPurchaseAmount = Console.readLine();
+        if (!inputLottoPurchaseAmount.matches("\\d+")){
+            throw new IllegalArgumentException("숫자를 입력해야합니다.");
+        }
+
+        int lottoPurchaseAmount = Integer.parseInt(inputLottoPurchaseAmount);
+        if (lottoPurchaseAmount%1000 != 0) {
+            throw new IllegalArgumentException("구입 금액은 1,000원 단위로 입력해야 합니다.");
+        }
+
+        
     }
 }
