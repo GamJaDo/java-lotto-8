774cbf3c2096591f29a40f93c01591b4b7ea0ec7
unknown <kim0615157@gmail.com>
2025-10-30 12:19:32 +0900

    feat: 로또 번호 리스트들 출력
    구입한 금액만큼 로또 번호 리스트들을 출력한다
    
    출력시 pickUniqueNumbersInRange 메서드를 사용하여 1-45 범위의 6자리 중복되지 않는 번호로 출력
    
    출력시 오름차순으로 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,8 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -9,14 +11,24 @@ public class Application {
         System.out.println("구입금액을 입력해 주세요.");
         String inputLottoPurchaseAmount = Console.readLine();
         if (!inputLottoPurchaseAmount.matches("\\d+")){
-            throw new IllegalArgumentException("숫자를 입력해야합니다.");
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해야합니다.");
         }
 
         int lottoPurchaseAmount = Integer.parseInt(inputLottoPurchaseAmount);
         if (lottoPurchaseAmount%1000 != 0) {
-            throw new IllegalArgumentException("구입 금액은 1,000원 단위로 입력해야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
         }
 
-        
+        int lottoPurchaseCount = lottoPurchaseAmount/1000;
+        System.out.println("\n" + lottoPurchaseCount + "개를 구매했습니다.");
+
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i=0; i<lottoPurchaseCount; i++) {
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            lottos.add(lotto);
+        }
+        for (Lotto lotto : lottos) {
+            lotto.printLottoNumber();
+        }
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -16,5 +18,12 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public void printLottoNumber() {
+        String joinNumber = numbers.stream()
+            .sorted()
+            .map(String::valueOf)
+            .collect(Collectors.joining(", "));
+
+        System.out.println("[" + joinNumber + "]");
+    }
 }
